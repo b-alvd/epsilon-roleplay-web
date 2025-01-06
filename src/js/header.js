9943cd004dab9fc372377e32e4833a3e9a6c6fcf
Basile <basilealeveque@gmail.com>
2025-01-06 21:05:17 +0100

    header + responsive

---
+++ src/js/header.js
@@ -0,0 +1,16 @@
+const hamburger = document.querySelector('header div#burger');
+const navMenu = document.querySelector('header ul');
+
+hamburger.addEventListener('click', () => {
+    if (navMenu.classList.contains('nav--active')) {
+        navMenu.classList.add('nav--active-closing');        
+        navMenu.addEventListener('animationend', () => {
+            navMenu.classList.remove('nav--active');
+            navMenu.classList.remove('nav--active-closing');
+            navMenu.style.display = 'none';
+        }, { once: true });
+    } else {
+        navMenu.style.display = 'flex';
+        navMenu.classList.add('nav--active');
+    }
+});
